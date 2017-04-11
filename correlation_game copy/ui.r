68f226cd6c3e4e1b88c18cac17690818eb59f999
sccastro <cac200222@gmail.com>
2017-04-11 16:46:54 -0600

    test

--- correlation_game copy/ui.r
+++ correlation_game copy/ui.r
@@ -1,42 +1,33 @@
-#ui.R
+# Load the ggplot2 package which provides
+# the 'mpg' dataset.
+library(ggplot2)
 
-
-require(shiny)
-require(ggplot2)
-require(ellipse)
-
-
-shinyUI(fluidPage(
+fluidPage(
+  titlePanel("Basic DataTable"),
   
-  tags$head(
-    tags$style(HTML("
-      @import url('//fonts.googleapis.com/css?family=Lobster|Cabin:400,700');
-    "))
-  ),
-  
-  titlePanel("Correlation Game"),
-  
+  # Create a new Row in the UI for selectInputs
   fluidRow(
-    column(width=4,
-           selectInput("difficulty",label="Difficulty",choices=list("Easy","Medium","Hard")),
-           h5(textOutput("score")),
-           br(),
-           checkboxGroupInput(inputId="options", label="Show:",choices=list("Averages","Standard deviation line","Ellipse")),
-           br(),
-           sliderInput("slider",label="The correlation between X and Y is...",min=-1,max=1,value=0,step=0.01),
-           br(),
-           p(textOutput("status1"),style="font-weight=500; color: #000000;"),
-           h5(textOutput("status2"),style="font-weight=500; color: #00CC00;"),
-           h5(textOutput("status3"),style="font-weight=500; color: #FF0000;"),
-           br(),
-           actionButton("submit","Submit"),
-           actionButton("newplot","New Plot")
-           ),
-    
-    column(width=8,
-           plotOutput("plot1",width="400px",height="400px"))
-            
+    column(4,
+           selectInput("man",
+                       "Manufacturer:",
+                       c("All",
+                         unique(as.character(mpg$manufacturer))))
+    ),
+    column(4,
+           selectInput("trans",
+                       "Transmission:",
+                       c("All",
+                         unique(as.character(mpg$trans))))
+    ),
+    column(4,
+           selectInput("cyl",
+                       "Cylinders:",
+                       c("All",
+                         unique(as.character(mpg$cyl))))
     )
-  
-  
-))
+  ),
+  # Create a new row for the table.
+  fluidRow(
+    DT::dataTableOutput("table")
+  )
+)

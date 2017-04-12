c9de349bd0b7cb80e46f4a4dcde7f5fe7afd4846
lacepadilla <lace.m.k.padilla@gmail.com>
2017-04-12 13:25:08 -0600

    changing ap

--- App-1/server.R
+++ App-1/server.R
@@ -1,8 +1,15 @@
-shinyServer(function(input, output) {
-  
-  output$text1 <- renderText({ 
-    paste("You have selected", input$range)
-  })
-  
-}
-)
+library(maps)
+library(mapproj)
+source("helpers.R")
+counties <- readRDS("counties.rds")
+
+shinyServer(
+  function(input, output) {
+    
+    output$map <- renderPlot({
+      
+      percent_map( # some arguments )
+    })
+      
+  }
+    )
--- App-1/ui.R
+++ App-1/ui.R
@@ -4,7 +4,7 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       helpText("Create demographic maps with 
-        information from the 2010 US Census."),
+               information from the 2010 US Census."),
       
       selectInput("var", 
                   label = "Choose a variable to display",
@@ -15,10 +15,8 @@ shinyUI(fluidPage(
       sliderInput("range", 
                   label = "Range of interest:",
                   min = 0, max = 100, value = c(0, 100))
-    ),
+      ),
     
-    mainPanel(
-      textOutput("text1")
-    )
+    mainPanel(plotOutput("map"))
   )
 ))

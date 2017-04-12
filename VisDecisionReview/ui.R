ee371afcf957bf0950176361cf0188063db1802c
lacepadilla <lace.m.k.padilla@gmail.com>
2017-04-12 14:25:30 -0600

    update

--- VisDecisionReview/ui.R
+++ VisDecisionReview/ui.R
@@ -12,18 +12,6 @@ fluidPage(
                        "Manufacturer:",
                        c("All",
                          unique(as.character(mpg$manufacturer))))
-    ),
-    column(4,
-           selectInput("trans",
-                       "Transmission:",
-                       c("All",
-                         unique(as.character(mpg$trans))))
-    ),
-    column(4,
-           selectInput("cyl",
-                       "Cylinders:",
-                       c("All",
-                         unique(as.character(mpg$cyl))))
     )
   ),
   # Create a new row for the table.

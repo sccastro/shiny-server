6603e6e8b58e39087cb158ffab6cbe4064a053b2
lacepadilla <lace.m.k.padilla@gmail.com>
2017-04-12 14:42:12 -0600

    update

--- VisDecisionReview/ui.R
+++ VisDecisionReview/ui.R
@@ -11,26 +11,17 @@ fluidPage(
   title = 'Examples of DataTables',
   sidebarLayout(
     sidebarPanel(
-      conditionalPanel(
-        'input.dataset === "diamonds"',
-        checkboxGroupInput('show_vars', 'Columns in diamonds to show:',
-                           names(diamonds), selected = names(diamonds))
-      ),
+      
       conditionalPanel(
         'input.dataset === "mtcars"',
         helpText('Click the column header to sort a column.')
-      ),
-      conditionalPanel(
-        'input.dataset === "iris"',
-        helpText('Display 5 records by default.')
       )
     ),
     mainPanel(
       tabsetPanel(
         id = 'dataset',
-        tabPanel('diamonds', DT::dataTableOutput('mytable1')),
-        tabPanel('mtcars', DT::dataTableOutput('mytable2')),
-        tabPanel('iris', DT::dataTableOutput('mytable3'))
+       
+        tabPanel('mtcars', DT::dataTableOutput('mytable2'))
       )
     )
   )

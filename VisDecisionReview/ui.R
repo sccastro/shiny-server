68ff815025ea9817655d016507d3c5574d6b4ae2
lacepadilla <lace.m.k.padilla@gmail.com>
2017-04-12 15:46:42 -0600

    update

--- VisDecisionReview/ui.R
+++ VisDecisionReview/ui.R
@@ -14,14 +14,19 @@ fluidPage(
     sidebarPanel(
       conditionalPanel(
         'input.dataset === "mpg2"',
-        checkboxGroupInput('show_vars', 'Data Options',
+        checkboxGroupInput('show_vars', '',
                            c("Authors", "Year", "N"), selected = c("Authors", "Year", "N"))
       ), 
       conditionalPanel(
         'input.dataset === "mpg2"',
         checkboxGroupInput('show_vars', 'Study Type',
                            c("UP", "VDAR", "UWP"), selected = c("UP", "VDAR", "UWP"))
-      )
+      ),
+      conditionalPanel(
+        'input.dataset === "mpg2"',
+        checkboxGroupInput('show_vars', 'Data Type',
+                           c("Items", "Attributes", "Links", "Positions", "Grids"), selected = "Items")
+      ) 
     ),
     mainPanel(
       tabsetPanel(

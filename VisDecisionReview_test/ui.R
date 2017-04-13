0579a284a3fdaffa08fc95d4784f16046d7b3d74
lacepadilla <lace.m.k.padilla@gmail.com>
2017-04-12 18:17:34 -0600

    update

--- VisDecisionReview_test/ui.R
+++ VisDecisionReview_test/ui.R
@@ -14,9 +14,7 @@ fluidPage(
   # Create a new Row in the UI for selectInputs
   sidebarLayout(
     
-    column(4, offset = 4,
-           h2("4 offset 4")
-    ),      
+        
     column(4,
            selectInput("TaskType",
                        "Task Type:",

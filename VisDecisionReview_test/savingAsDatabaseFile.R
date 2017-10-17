8b5bbce76a97a69d50f1b318f0d6205966c34767
Spencer Castro <cac200222@gmail.com>
2017-10-17 18:12:15 -0400

    Update

--- VisDecisionReview_test/savingAsDatabaseFile.R
+++
@@ -1,5 +0,0 @@
-#saving data file
-library(xlsx)
-total<- read.xlsx("~/Documents/SpencerServer/shiny-server/VisDecisionReview/Table2.xlsx", sheetName="Table2.csv")
-
-saveRDS(total, "~/Documents/SpencerServer/shiny-server/VisDecisionReview/data.rds")
